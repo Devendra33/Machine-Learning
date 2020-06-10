46b84b87f2d64422b2b670be392bfeba0dd69b79
Devendra33 <47396740+Devendra33@users.noreply.github.com>
2020-06-10 10:11:39 +0530

    Add files via upload

---
+++ ML(Code Basics)/Matplotlib/plot.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = [1,2,3,4,5,6,7]
+y = [50,51,59,53,61,48,45]
+
+plt.plot(x, y, color = "green", marker = "D", linestyle = "-", markersize = 10)
+
+
