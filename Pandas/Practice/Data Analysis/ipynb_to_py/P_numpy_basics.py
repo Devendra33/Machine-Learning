46d067fc36b15c9b7d9c807d586a1cf4d0c1451a
Devendra Gohare <47396740+Devendra33@users.noreply.github.com>
2020-07-31 00:12:26 +0530

    Add files via upload

---
+++ Pandas/Practice/Data Analysis/ipynb_to_py/P_numpy_basics.py
@@ -0,0 +1,31 @@
+# practising the numpy basics!
+
+import numpy as np
+
+# creating 1D and 2D Arrays
+
+
+# Dimention of array
+       # 1
+       # 2
+
+     # tells the data type of array
+     # tells the row x col
+     # number of elements in the array (6)
+
+# to change the shape of array
+      # ROWS * COLS  = Number of elements in the array (MUST CONDITION)
+
+# To convert N-Dimention array to 1D array
+  
+
+# min and max operation
+    
+    
+    # axis 0 for column and 1 for row
+
+# for mathematical operation
+       # return matrix of square rooted elements 
+       # standard deviation of all element in the matrix
+
+# if we apply +,-,*,/ on numpy array(dimention must be same else error.) then it performs mathematical operations
