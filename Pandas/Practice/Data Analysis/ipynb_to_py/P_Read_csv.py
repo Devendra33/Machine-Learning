f90f9022af653ef8ab4687e1d81daa6d634ddeef
Devendra Gohare <47396740+Devendra33@users.noreply.github.com>
2020-07-31 00:14:33 +0530

    Add files via upload

---
+++ Pandas/Practice/Data Analysis/ipynb_to_py/P_Read_csv.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+# use player_data.csv
+
+# Reading a file
+col =  ['index', 'nbaDebutYear', 'firstName', 'lastName', 'jersey',
+       'heightMeters', 'weightKilograms', 'int_dt'] 
+      # reading  a file with column titles
+      # can be used for skipping the rows,
+                                                # header = 0, it is default
+                                                # if header = 1 then first row become title
+
+                      # to read nunber of n number of rows
+
+        # it makes null value, key == column name
+                                                                                    #value == to make it null
+                                                                                        
+# Now Writing into a file.
+
+# index = False : now default index will not be visible on the file,
+# columns =['index', 'firstName', 'lastName'] :  only these columns will visile on new file,
+# header = False : column title will not be visible
+
+
+                                                                                        
+
