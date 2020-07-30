6529200f65ba218463fd566159847401e691c73a
Devendra Gohare <47396740+Devendra33@users.noreply.github.com>
2020-07-31 00:13:01 +0530

    Add files via upload

---
+++ Pandas/Practice/Data Analysis/ipynb_to_py/P_pandas_groupby.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+ds = pd.read_csv("wheather.csv")
+
+# now applying the groupby method
+    
+# for selecting a perticular city domain
+
+
+
+       # gives all details of mumbai  city
+
+        # gives the maximum value of cities
+        # gives the minimum value of cities
+        # gives  the averag
+
+      # gives all the details of all the cities.
+
+     # it will plot the graph for each group.
